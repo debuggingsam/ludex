e6a28e15a16deafc68a1bca4732113b43bededc9
SamA673 <saminahmeddawson@gmail.com>
2025-02-18 18:50:32 -0500

    Add query functions to query.ts

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -3,4 +3,36 @@ import { Context } from "./context";
 
 export const Query: IQuery<Context> = {
   hello: () => "world",
+
+  
+  getAllTodos: async (_: any, __: any, { prisma }: Context) => {
+    return await prisma.todo.findMany();
+  },
+
+
+  getTodoById: async (_: any, args: { id: string }, { prisma }: Context) => {
+    return await prisma.todo.findUnique({
+      where: {
+        id: args.id,
+      },
+    });
+  },
+
+
+  getCompletedTodos: async (_: any, __: any, { prisma }: Context) => {
+    return await prisma.todo.findMany({
+      where: {
+        completed: true,
+      },
+    });
+  },
+
+
+  getIncompleteTodos: async (_: any, __: any, { prisma }: Context) => {
+    return await prisma.todo.findMany({
+      where: {
+        completed: false,
+      },
+    });
+  },
 };
