606459cf4b8f9387715812dfdda1e46752dc54d0
SamA673 <saminahmeddawson@gmail.com>
2025-02-18 19:16:38 -0500

    Add validation to mutations. (Untested)

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -20,6 +20,10 @@ export const Mutation: IMutation<Context> = {
 
   // CREATION
   createTodo: async (_, { input }, { prisma }) => {
+    if (!input.title) {
+      throw new Error("You forgot to add a title to your todo.");
+    }
+
     const newTodo = await prisma.todo.create({
       data: {
         title: input.title,
@@ -31,17 +35,33 @@ export const Mutation: IMutation<Context> = {
 
     // MARK COMPLETION via ID 
     markTodoAsComplete: async (_, { id }, { prisma }) => {
+
+      // Get the todo (or undefined)
+      const todo = await prisma.todo.findUnique({ where: { id } });
+
+      // no todo found
+      if (!todo) {
+        throw new Error(`The id you gave (${id}) doesn't match any todos in our db.`);
+      }
+  
       const updatedTodo = await prisma.todo.update({
         where: { id },
-        data: {
-          completed: true,
-        },
+        data: { completed: true },
       });
+  
       return updatedTodo;
     },
 
     // NAME CHANGE via ID AND NEW NAME 
     updateTodoTitle: async (_, { id, title }, { prisma }) => {
+      // Get the todo (or undefined)
+      const todo = await prisma.todo.findUnique({ where: { id } });
+
+      // no todo found
+      if (!todo) {
+        throw new Error(`The id you gave (${id}) doesn't match any todos in our db.`);
+      }
+
       const updatedTodo = await prisma.todo.update({
         where: { id },
         data: {
@@ -55,6 +75,16 @@ export const Mutation: IMutation<Context> = {
 
   // DELETION via ID
   deleteTodo: async (_, { id }, { prisma }) => {
+      // Get the todo (or undefined)
+      const todo = await prisma.todo.findUnique({ where: { id } });
+
+      // no todo found
+      if (!todo) {
+        throw new Error(`you're trying to delete a nonesxistant todo... `);
+      }
+
+
+
     const deletedTodo = await prisma.todo.delete({
       where: { id },
     });
